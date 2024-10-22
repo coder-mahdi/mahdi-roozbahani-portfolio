77535f3a121d08b196e4be0d448bfb919dd28808
mahdi-mim <mahdiroozbahani25@gmail.com>
2024-10-22 14:18:33 -0700

    added contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,6 @@ function Contact() {
   const [message, setMessage] = useState('');
   const [submitted, setSubmitted] = useState(false);
 
- 
   useEffect(() => {
     fetch('/data/contactData.json')
       .then((response) => response.json())
@@ -23,24 +22,33 @@ function Contact() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    fetch('http://localhost:5000/send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, message }), 
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Success:', data);
-        setSubmitted(true); 
-      })
-      .catch((error) => {
-        console.error('Error:', error);
+    window.grecaptcha.ready(() => {
+      window.grecaptcha.execute('6LclDWkqAAAAAAYPl_MAvzZrVo3DlB4nYy39S4El', { action: 'submit' }).then((token) => {
+   
+        fetch('https://mahdiroozbahani.com/save_message.php', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: new URLSearchParams({
+            name,
+            email,
+            message,
+            recaptchaToken: token,
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('Success:', data);
+            setSubmitted(true);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
       });
+    });
   };
 
-  
   if (!contactData) {
     return <div>Loading...</div>;
   }
@@ -51,50 +59,56 @@ function Contact() {
     
         <div className="welcome-message-wrapper">
           <TypewriterText text={contactData.welcomeMessage} /> 
+        <div className="email-button-wrapper">
+          <a href="mailto:hello@mahdiroozbahani.com" className="email-button">
+            Send Email
+          </a>
+        </div>
         </div>
 
 
         <div className="contact-form-wrapper">
-          {submitted ? (
+          {submitted && (
             <div className="form-success">
-              {contactData.successMessage} 
+              {contactData.successMessage || "Your message was successfully sent!"}
             </div>
-          ) : (
-            <form className="contact-form" onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="name">{contactData.formLabels.name}</label>
-                <input
-                  type="text"
-                  id="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">{contactData.formLabels.email}</label>
-                <input
-                  type="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="message">{contactData.formLabels.message}</label>
-                <textarea
-                  id="message"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  required
-                ></textarea>
-              </div>
-              <button type="submit" className="submit-btn">
-                {contactData.formLabels.submit}
-              </button>
-            </form>
           )}
+          
+          <form className="contact-form" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">{contactData.formLabels.name}</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">{contactData.formLabels.email}</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">{contactData.formLabels.message}</label>
+              <textarea
+                id="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              ></textarea>
+            </div>
+
+            <button type="submit" className="submit-btn">
+              {contactData.formLabels.submit}
+            </button>
+          </form>
         </div>
       </div>
     </Layout>
